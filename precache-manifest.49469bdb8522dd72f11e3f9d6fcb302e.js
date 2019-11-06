47b9677fd49f670a7bc8864f77a0e07d511ef984
ayauri29 <albayauri.29@gmail.com>
2019-11-06 16:43:53 -0500

    Updates

---
+++ precache-manifest.49469bdb8522dd72f11e3f9d6fcb302e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717cb1a659a490b001ce9eb646464231",
+    "url": "/Hackaton-interna/index.html"
+  },
+  {
+    "revision": "0f5745cba47c02635a59",
+    "url": "/Hackaton-interna/static/css/main.251f3277.chunk.css"
+  },
+  {
+    "revision": "f88b1044635dce04d227",
+    "url": "/Hackaton-interna/static/js/2.4af3a8ff.chunk.js"
+  },
+  {
+    "revision": "0f5745cba47c02635a59",
+    "url": "/Hackaton-interna/static/js/main.7a7da449.chunk.js"
+  },
+  {
+    "revision": "3be2b085630d66f6e8e6",
+    "url": "/Hackaton-interna/static/js/runtime-main.91a4bbcb.js"
+  },
+  {
+    "revision": "e3b2509b56ae8c218ed47f9828de7836",
+    "url": "/Hackaton-interna/static/media/img1.e3b2509b.jpeg"
+  },
+  {
+    "revision": "e498f22a36f441aff37a2f61c0652f59",
+    "url": "/Hackaton-interna/static/media/img2.e498f22a.jpeg"
+  }
+]);
